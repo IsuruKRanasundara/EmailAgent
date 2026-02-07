2edb45b4761174139df9febe73ed9b642a5fa188
IsuruKRanasundara <ranasundaraisuru4@gmail.com>
2026-02-07 07:36:32 +0530

    New Changes

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 # config.py
 EMAIL_CONFIG = {
-    'email': 'ranasundaraisuru4@gmail.com',
-    'password': '10IkR@2002',  # 16-character app password
+    'email': 'email@gmail.com',
+    'password': 'password',  # 16-character app password
     'imap_server': 'imap.gmail.com',
     'smtp_server': 'smtp.gmail.com',
     'smtp_port': 587
 }
 HUGGINGFACE_CONFIG = {
-    'token': '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'  # Your token here
+    'token': 'token'  # Your token here
 }
